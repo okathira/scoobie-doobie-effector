4c5a673d2310890b8aa425416fe00be25b998edb
okathira <43328895+okathira@users.noreply.github.com>
2021-01-01 03:03:15 +0900

    カメラの映像を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useRef, useCallback, useMemo } from "react";
+import Webcam from "react-webcam";
+import { Stage, Layer } from "react-konva";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user",
+};
+
+const layoutSize = {
+  width: 640,
+  height: 360,
+};
 
 const App: React.FC = () => {
   return <section className="App">app</section>;
+  return (
+    <section className="App">
+      <Webcam
+        id="camera"
+        audio={false}
+        videoConstraints={videoConstraints}
+        width={layoutSize.width}
+        height={layoutSize.height}
+      />
+      <Stage id="outputArea">
+        <Layer></Layer>
+      </Stage>
+    </section>
+  );
 };
 
 export default App;
