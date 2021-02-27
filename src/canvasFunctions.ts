a8d7c65b7db427a4170e6184b1fbf530effcaf90
okathira <43328895+okathira@users.noreply.github.com>
2021-02-28 03:20:01 +0900

    色をそれっぽくする

--- src/canvasFunctions.ts
+++ src/canvasFunctions.ts
@@ -8,15 +8,17 @@ export const createCanvas = (size: RectSize) => {
 
 // グレイスケールならCSSでもできる。その場合はcanvasごとにフィルターがかかるため構造を変える必要がありそう。
 export const makeGrayscale = (image: ImageData) => {
-  const l = image.data.length / 4;
+  const l = image.data.length / 4; // RGBA
   for (let i = 0; i < l; i++) {
     const grayscale =
       image.data[i * 4 + 0] * 0.299 +
       image.data[i * 4 + 1] * 0.587 +
       image.data[i * 4 + 2] * 0.114;
 
-    image.data[i * 4 + 0] = grayscale;
-    image.data[i * 4 + 1] = grayscale;
-    image.data[i * 4 + 2] = grayscale;
+    const s = (grayscale * grayscale) / 255 / 255;
+
+    image.data[i * 4 + 0] = (s + 0.008) * 255;
+    image.data[i * 4 + 1] = (s + 0.014) * 255 + 1.9;
+    image.data[i * 4 + 2] = (s + 0.004) * 255;
   }
 };
