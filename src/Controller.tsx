cfc1be142525b6325a167e65129fc4858a19797c
okathira <43328895+okathira@users.noreply.github.com>
2021-03-04 06:42:37 +0900

    反転ボタンで反転を可能にした

--- src/Controller.tsx
+++ src/Controller.tsx
@@ -7,13 +7,55 @@ const Controller: React.FC<{
     React.Dispatch<React.SetStateAction<number | undefined>>
   ];
 }> = ({ selectedKeyState }) => {
-  const [selectedKey, setSelectedKey] = selectedKeyState;
+  const [selectedKey] = selectedKeyState;
   const setBoxContainer = useSetBoxContainers();
+
+  // 選択されたClippingBoxのboxPropsを書き換える関数を返す
+  const affectSelectedBox = (
+    changeProps: (preProps: BoxProps) => BoxProps
+  ) => () => {
+    if (selectedKey === undefined) {
+      console.error("No Boxes Selected");
+      return null;
+    }
+
+    setBoxContainer((preState) => {
+      const preProps = preState.get(selectedKey);
+      if (preProps === undefined) {
+        console.error("Not Found");
+        return preState;
+      }
+
+      const newProps: BoxProps = changeProps(preProps);
+      const newState = new Map(preState);
+      return newState.set(selectedKey, newProps);
+    });
+  };
+
+  const flipHorizontally = (preProps: BoxProps) => ({
+    ...preProps,
+    scaleX: -preProps.scaleX,
+  });
+
+  const flipVertically = (preProps: BoxProps) => ({
+    ...preProps,
+    scaleY: -preProps.scaleY,
+  });
 
   return (
     <div>
-      <button>左右反転</button>
-      <button>上下反転</button>
+      <button
+        onClick={affectSelectedBox(flipHorizontally)}
+        disabled={selectedKey === undefined}
+      >
+        左右反転
+      </button>
+      <button
+        onClick={affectSelectedBox(flipVertically)}
+        disabled={selectedKey === undefined}
+      >
+        上下反転
+      </button>
     </div>
   );
 };
